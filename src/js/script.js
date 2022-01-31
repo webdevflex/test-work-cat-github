d5297db59efe0179bf9e64dafd02e6ea18200292
webdevflex <andreyweb30@gmail.com>
2022-01-31 16:22:56 +0200

    wew

---
+++ src/js/script.js
@@ -0,0 +1,99 @@
+// burger-menu
+// burger-menu-open
+let btnBurger = document.querySelector('.gamburger_menu');
+let menuOpen = document.querySelector('.menu__list');
+let title = document.querySelector('.title');
+let headerImg = document.querySelector('.header-content__images')
+
+btnBurger.addEventListener('click', function(){
+    btnBurger.classList.toggle('burger_active');
+    menuOpen.classList.toggle('menu_list_active')
+    title.classList.toggle('title_active')
+    headerImg.classList.toggle('title_active')
+});
+
+
+
+
+//   phone mask
+  $("#phone").click(function(){
+    $(this).setCursorPosition(3);
+  }).mask("+3(099) 999-9999");
+  $("#phone").mask("+38(099) 999-9999");
+
+
+
+  // при клике для ввода номера, курсор всегда будет в начале 
+$.fn.setCursorPosition = function(pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+    }
+  };
+
+  //   вывод даты
+var now = new Date().toLocaleDateString();
+let outDate = document.querySelector('.out-date');
+  outDate.innerHTML = now;
+
+  // таймер
+  document.addEventListener('DOMContentLoaded', function () {
+    // конечная дата
+    const deadline = new Date(Date.now() + 7200000);
+    // id таймера
+    let timerId = null;
+    // склонение числительных
+    function declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+    
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+    
+      $hours.textContent = hours < 10 ? '0' + hours : hours;
+      $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+      $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+    
+      $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+      $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+      $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+    }
+    // получаем элементы, содержащие компоненты даты
+
+    const $hours = document.querySelector('.timer__hours');
+    const $minutes = document.querySelector('.timer__minutes');
+    const $seconds = document.querySelector('.timer__seconds');
+    // вызываем функцию countdownTimer
+    countdownTimer();
+    // вызываем функцию countdownTimer каждую секунду
+    timerId = setInterval(countdownTimer, 1000);
+  });
+
+  // плавный скрол к якорю
+  const anchors = document.querySelectorAll('a[href*="#"]')
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+      
+      const blockID = anchor.getAttribute('href').substr(1)
+      
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
+	
